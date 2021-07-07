7e2071a48ef609a25f4e5de59019671b0236dfc5
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2021-07-07 11:45:31 -0400

    types and variables

---
+++ introDatatypes.py
@@ -0,0 +1,22 @@
+#boolean
+is_active = False
+print(is_active)
+#ints
+num = 7
+print(num)
+#string
+name = 'Jeevers'
+print(name)
+#list
+my_list = [1, 2, 3]
+print(my_list)
+#dictionary (think object)
+ledger = {'debit': 7, 'credit': 8}
+print(ledger)
+#dynamic typing, can reassign ad nauseum 
+cool = True
+print(cool)
+cool = 7
+print(cool)
+cool = 7+7
+print(cool)
---
+++ introVariables.py
@@ -0,0 +1,5 @@
+name = 'Sanjeev'
+print(name)
+num = 7
+print(num)
+print(num + 1)
